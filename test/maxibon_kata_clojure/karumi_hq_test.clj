aab82f021bfe2806ef166fd309e08a7c0dd62fe3
Fernando Garcia Samblas <fernando.garcia@the-cocktail.com>
2020-08-20 12:54:09 +0200

    Remove tests of karumi_hq code

--- test/maxibon_kata_clojure/karumi_hq_test.clj
+++ test/maxibon_kata_clojure/karumi_hq_test.clj
@@ -4,24 +4,11 @@
             [clojure.test.check.properties :as prop]
             [maxibon-kata-clojure.karumi_hq :refer :all]))
 
-;;;;;;;;;;;;;;;;
-;; generators ;;
-;;;;;;;;;;;;;;;;
+;; generators
 
-(def gen-non-empty-string
-  (let [chars "abcdefghijklmnopqrstuvwxyz0123456789"]
-    (gen/let [cs (gen/list (gen/elements chars))]
-      (apply str (rand-nth chars) cs))))
+;; Write here the generators required by your tests... (see
+;; http://clojure.org/guides/test_check_beginner#_generators)
 
-(def gen-office
-  (gen/let [name gen-non-empty-string]
-    (create-office name)))
+;; specs
 
-;;;;;;;;;;;
-;; specs ;;
-;;;;;;;;;;;
-
-(defspec office-should-start-the-day-with-at-least-10-maxibons
-         100
-         (prop/for-all [office gen-office]
-           (= 10 (:maxibons office))))
+;; ...and here your code specs with test.check!!
