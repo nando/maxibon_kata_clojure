9815b54b5dc1f2fa35d869005e7f71643b889b27
Fernando Garcia Samblas <fernando.garcia@the-cocktail.com>
2020-08-20 11:26:40 +0200

    remove test code from this branch (as README says)

--- test/maxibon_kata_clojure/developer_test.clj
+++ test/maxibon_kata_clojure/developer_test.clj
@@ -5,21 +5,10 @@
             [maxibon-kata-clojure.developer :refer :all]))
 
 ;; generators
-(def gen-developer-index
-  (gen/choose 0
-              (dec (count developers))))
 
-(def gen-developer-name
-  (gen/elements (names)))
+;; Write here the generators required by your tests... (see
+;; http://clojure.org/guides/test_check_beginner#_generators)
 
 ;; specs
-(defspec info-function-searchs-developer-by-name
-  100
-  (prop/for-all [index gen-developer-index]
-    (let [dev (nth developers index)]
-      (= dev (info (:name dev))))))
 
-(defspec names-function-returns-valid-developer-names
-  100
-  (prop/for-all [name gen-developer-name]
-    (some #(= name (:name %)) developers)))
+;; ...and here your code specs with test.check!!
