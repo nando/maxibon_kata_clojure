d268e7ee71e954112505cb2b5d2f86b1429446ee
Fernando Garcia Samblas <fernando.garcia@the-cocktail.com>
2020-08-25 09:00:31 +0200

    Add :csv to developer hash-map

--- src/maxibon_kata_clojure/csv.clj
+++ src/maxibon_kata_clojure/csv.clj
@@ -21,28 +21,29 @@
 
 (defn mapify
   "Return a seq of maps like {:name \"Edward Cullen\" :grabs 10}"
-  [rows]
+  [team_name rows]
   (map (fn [unmapped-row]
          (reduce (fn [row-map [map-key value]]
                    (assoc row-map map-key (convert map-key value)))
-                 {}
+                 { :team team_name }
                  (map vector developer-keys unmapped-row)))
        rows))
 
 (defn csv2hash-map
-  [ filename ]
+  [ team_name ]
   (mapify
+    team_name
     (parse
-      (slurp filename))))
+      (slurp (str "./data/" team_name ".csv")))))
 
 (defn load_developers
   "Returns a vector with developer team names (Karumies if team_name not present!!)"
   ([]
      (load_developers "Karumies"))
   ([ team_name ]
-     (csv2hash-map (str "./data/" team_name ".csv")))
+     (csv2hash-map team_name))
   ([ team_name & other_teams]
-     (concat (csv2hash-map (str "./data/" team_name ".csv"))
+     (concat (csv2hash-map team_name)
              (apply load_developers other_teams))))
 
 (def memoized-developers (memoize load_developers))
