6d4dc9d3ce7fd268e9feea1c39a63dbdf67587b2
Fernando Garcia Samblas <fernando.garcia@the-cocktail.com>
2020-08-26 11:53:48 +0200

    Green build and... add Rocío to Rubyroomrer.csv!! ;)

--- src/maxibon_kata_clojure/karumi_hq.clj
+++ src/maxibon_kata_clojure/karumi_hq.clj
@@ -1,6 +1,8 @@
 (ns maxibon-kata-clojure.karumi_hq
   (:require [maxibon-kata-clojure.developer :as dev :refer :all]))
 
+(def maxibons-to-buy 10)
+
 (defn create-office
   "Creates a new office for Karumi with the name received (:madrid if omitted)"
   ([]
@@ -12,6 +14,9 @@
 (defn open-fridge
   "One developer opens the office's fridge..."
   ([office developer]
-    { :name (:name office),
-      :maxibons (- (:maxibons office)
-                   (:grabs developer)) }))
+    (let [maxibons-left (max 0 (- (:maxibons office)
+                               (:grabs developer)))]
+      { :name (:name office),
+        :maxibons (if (> maxibons-left 2)
+                     maxibons-left
+                     (+ maxibons-left maxibons-to-buy)) })))
